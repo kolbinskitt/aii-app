ecbf2a0a6d74724ff25cb23418c5e08d46c31006
Krzysztof Olbinski <olbik80@gmail.com>
2026-01-02 14:54:25 +0100

    Fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from '/components/App.jsx'
+import NotFound from '/components/NotFound.jsx'
 
-const App = () => {
-  return <h1>Aii app started</h1>
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/aii-app/',
+  base: process.env.NODE_ENV === 'production' ? '/aii-app/' : '/',
   plugins: [react()],
 })
