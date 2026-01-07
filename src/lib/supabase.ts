ba3bfcd828001c0cd9d48cb0af1df726eaf44baa
Krzysztof Olbinski <olbik80@gmail.com>
2026-01-07 15:12:15 +0100

    ...

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -7,7 +7,7 @@ export const supabase = createClient(
     auth: {
       autoRefreshToken: true,
       persistSession: true,
-      detectSessionInUrl: false,
+      detectSessionInUrl: true,
       flowType: 'pkce',
     },
   },
