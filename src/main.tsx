77a253d1457259afc2057dc86611e86b58be1615
Krzysztof Olbinski <olbik80@gmail.com>
2026-01-07 14:52:17 +0100

    ...

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { RouterProvider, HashRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import { UserProvider } from './context/UserContext';
 import { router } from './router';
 import './index.css';
 
 const queryClient = new QueryClient();
-
-const redirect = sessionStorage.redirect;
-delete sessionStorage.redirect;
-
-if (redirect) {
-  window.history.replaceState(null, '', redirect);
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
